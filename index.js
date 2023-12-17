fe4a4a3de1e3646976348316175166a826005466
Levi016 <150278720+Levi016@users.noreply.github.com>
2023-12-17 11:25:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,48 @@ window.onscroll = function() {
 document.addEventListener("DOMContentLoaded", function() {
   const button = document.querySelector('button');
   const paragraph = document.createElement('p');
-  paragraph.textContent = "Urbanização é o processo de crescimento e desenvolvimento das áreas urbanas, envolvendo a migração da população do campo para a cidade, a construção de infraestrutura urbana, o surgimento de novos estilos de vida e a transformação do ambiente físico e social.";
+  paragraph.textContent = "n sei fazer isso D:.";
   
   button.addEventListener('click', function() {
       document.getElementById('home').appendChild(paragraph);
-      button.style.display = 'none'; // Esconde o botão após clicar
+      button.style.display = 'none'; 
   });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  
+  showContent("historia");
+
+  
+  function showContent(sectionId) {
+      const sections = document.querySelectorAll(".content");
+      sections.forEach((section) => {
+        section.style.display = "none";
+      });
+  
+      const selectedSection = document.getElementById(sectionId);
+      selectedSection.style.display = "block";
+    }
+  
+   
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const sectionId = link.getAttribute("href").substring(1);
+        showContent(sectionId);
+      });
+    });
+  });
+  
+const saibaMaisBtn = document.getElementById('saibaMaisBtn');
+const infoAdicional = document.getElementById('infoAdicional');
+
+
+saibaMaisBtn.addEventListener('click', function() {
+    
+    if (infoAdicional.style.display === 'none') {
+        infoAdicional.style.display = 'block';
+    } else {
+        infoAdicional.style.display = 'none';
+    }
 });
